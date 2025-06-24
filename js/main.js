fee6f4e19e3bf511dc653db26238c1a594ce529e
MaksimZaichenko <maxzaichenko@gmail.com>
2025-06-24 21:56:52 +0300

    start JavaScript code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+'use strict'
 
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item')
+
+tabItem.forEach(function(element){
+    element.addEventListener('click', open)
 });
+
+function open(evt) {
+    const tabTarget = evt.currentTarget;
+    const
+}
